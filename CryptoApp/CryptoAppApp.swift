567a8ca0b7c9fb109604fbd1931e7fcc915d1a0e
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-09-01 17:58:00 -0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Boris R on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
