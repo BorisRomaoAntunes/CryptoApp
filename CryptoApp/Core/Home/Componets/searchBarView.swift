b29820b91fab1f4cc85c8e07e264027a57171fb2
Boris Romao Antunes <romaoboris@gmail.com>
2023-09-29 18:29:30 -0300

    implementando barra de search no app

---
+++ CryptoApp/Core/Home/Componets/searchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  searchBarView.swift
+//  CryptoApp
+//
+//  Created by Boris Romão Antunes on 29/09/23.
+//
+
+import SwiftUI
+
+struct searchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    searchBarView()
+}
---
+++ CryptoApp/Utilites/LocalFileManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocalFileManager.swift
+//  CryptoApp
+//
+//  Created by Boris Romão Antunes on 26/09/23.
+//
+
+import Foundation
